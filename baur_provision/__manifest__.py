b2a8d3cc84791099a02340eb9a1fe639977adbe5
Ashvin Rathod <ajrathod101@gmail.com>
2023-07-17 18:06:24 +0530

    [IMP] added baur provision

---
+++ baur_provision/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Powered by Mindphin Technologies.
+{
+    'name': 'Baur Provision',
+    'version': '1.0',
+    "summary": '',
+    'description': """ """,
+    "category": "Sales",
+    'license': 'OPL-1',
+    'author': 'Kanak Infosystems LLP.',
+    'website': 'https://www.kanakinfosystems.com',
+    'images': '',
+    'depends': ['sale_management', 'ons_productivity_qrcode_scan', 'account'],
+    'data': [
+        'views/sale.xml',
+        'report/sale_report_views.xml',
+    ],
+    'installable': True,
+}
---
+++ baur_provision/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Powered by Mindphin Technologies.
+
+from . import sale
