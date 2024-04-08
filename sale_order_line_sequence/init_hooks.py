d5c7990d95cc2e24e986acb581fb3ad902279927
Ashvin Rathod <ajrathod101@gmail.com>
2024-04-08 15:58:02 +0530

    [IMP] added migrated app

--- sale_order_line_sequence/init_hooks.py
+++ sale_order_line_sequence/init_hooks.py
@@ -6,10 +6,10 @@ from odoo import SUPERUSER_ID
 from odoo.api import Environment
 
 
-def post_init_hook(cr, pool):
+def post_init_hook(env):
     """
     Fetches all the sale order and resets the sequence of the order lines
     """
-    env = Environment(cr, SUPERUSER_ID, {})
+    # env = Environment(SUPERUSER_ID, {})
     sale = env["sale.order"].search([])
     sale._reset_sequence()
